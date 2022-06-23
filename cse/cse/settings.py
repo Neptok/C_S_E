a809a071f09147c136159c3a2fdac45a3492bd71
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-06-23 12:01:05 +0545

    Some setting with static and template configurattion

--- cse/cse/settings.py
+++ cse/cse/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"main/templates",'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#  user defined
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+    BASE_DIR/'main/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
