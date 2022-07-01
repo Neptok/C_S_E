9b2e2e411d12a97fb15e3d5c2fd9c2acd693cd92
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-07-01 12:20:02 +0545

    some changes

--- cse/cse/settings.py
+++ cse/cse/settings.py
@@ -116,12 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 #  user defined
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-    BASE_DIR/'main/static'
+    BASE_DIR / "static/",
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
